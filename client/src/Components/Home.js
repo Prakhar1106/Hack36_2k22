3a9790964ededdb288db9eff898f6baf78992897
coder-oj <ojasviguptaoj@gmail.com>
2022-04-24 02:51:25 +0530

    minor fixes

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -8,7 +8,7 @@ const Home = ({ user }) => {
       <h3>Welcome, {user.user.name}</h3>
 
       <SafeZone user={user} />
-      <Fir />
+      <Fir user={user}/>
       <Sos user={user} />
     </div>
   );
