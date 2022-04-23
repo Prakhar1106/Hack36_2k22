1f36fd5b07a3a96ee975b09ea59614de6e8c1fee
coder-oj <ojasviguptaoj@gmail.com>
2022-04-24 02:34:00 +0530

    some routes added

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
-const {signup, login, logout} = require("../controllers/auth");
+const {signup, login, logout, userinfo} = require("../controllers/auth");
 
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/signup', signup);                 //custom signup
-router.post('/login',login);   
+router.post('/login',login);  
+router.post('/logout', logout); 
+router.post('/userinfo',userinfo);
 
 module.exports = router;
